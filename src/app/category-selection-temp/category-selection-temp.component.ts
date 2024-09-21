83eb562f5508fa3c5b608603178dc3041949e363
bar arvili <bararvili2@gmail.com>
2024-09-21 13:22:19 +0300

    Standards changes 6

--- src/app/category-selection-temp/category-selection-temp.component.ts
+++ src/app/category-selection-temp/category-selection-temp.component.ts
@@ -29,8 +29,8 @@ import { RouterModule } from '@angular/router';
     FormsModule,
     RouterModule,
   ],
-  templateUrl: './category-selection.component.html',
-  styleUrl: './category-selection.component.css',
+  templateUrl: './category-selection-temp.component.html',
+  styleUrl: './category-selection-temp.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategorySelectionComponent implements OnInit {
